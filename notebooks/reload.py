b59eb9321a405afd441e5095884275372f0f039f
Zijun Zhang <zzj.zju@gmail.com>
2022-01-12 09:38:49 -0500

    No indel model and search

--- notebooks/reload.py
+++ notebooks/reload.py
@@ -5,10 +5,10 @@ import pickle
 import os
 from amber.utils import run_from_ipython
 
-def reload_from_dir(wd, replace_conv_by_fc=True):
+def reload_from_dir(wd, replace_conv_by_fc=True, n_channels=13):
     model_params = pickle.load(open(os.path.join(wd, "AmberSearchBestModel_config.pkl"), "rb"))
     kinn = KineticModel(model_params)
-    mb = KineticNeuralNetworkBuilder(kinn=kinn, n_channels=13, replace_conv_by_fc=replace_conv_by_fc)
+    mb = KineticNeuralNetworkBuilder(kinn=kinn, n_channels=n_channels, replace_conv_by_fc=replace_conv_by_fc)
     mb.build()
     mb.model.load_weights(os.path.join(wd, "AmberSearchBestModel.h5"))
     mb.model.summary()
